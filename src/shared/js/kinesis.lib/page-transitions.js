03abb3b30b00fea798fd4064db7ad3239ab5607e
Tyler <tyschemmel@gmail.com>
2018-03-27 14:58:12 -0500

    removing files

--- src/shared/js/kinesis.lib/page-transitions.js
+++
@@ -1,2 +0,0 @@
-setTimeout(function(){document.getElementsByTagName("body")[0].classList.remove("disableclick")}, 500);
-setTimeout(function(){pagecontenttransition.classList.remove('hidecontent')}, 10)
